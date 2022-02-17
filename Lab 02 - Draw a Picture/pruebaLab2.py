2934aed936d119272a8bcf7ec156cfccf1023b14
Chenyu Castillo <chenyu.castillo@edu.uah.es>
2022-02-17 18:44:48 +0100

    pruebaLab2

--- Lab 02 - Draw a Picture/pruebaLab2.py
+++ Lab 02 - Draw a Picture/pruebaLab2.py
@@ -1,28 +1,41 @@
 import arcade
 
+#creación de la ventana donde va a estar nuetro dibujo
+#800x600
 arcade.open_window(800, 600, "Drawing Example")
 
+#color de fondo de nuestra ventana
 arcade.set_background_color(arcade.color.BABY_BLUE)
 
 arcade.start_render()
 
+#creación del sol
 arcade.draw_circle_filled(725, 525, 50, arcade.color.ORANGE)
 
+#creación del suelo donde se apoya la casa
 arcade.draw_lrtb_rectangle_filled(0, 800, 150, 0, arcade.color.BANGLADESH_GREEN)
 
+#creación de la estructura de la casa(parte rectangular)
 arcade.draw_lrtb_rectangle_filled(200, 400, 380, 125, arcade.color.BLUE_GREEN)
 
+#creación de la ventanas(primer cuadrado--> exterior, segundo cuadrado-->interior)
 arcade.draw_lrtb_rectangle_filled(225, 275, 335, 285, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
 arcade.draw_lrtb_rectangle_filled(230, 270, 330, 290, arcade.color.BABY_BLUE_EYES)
 
 arcade.draw_lrtb_rectangle_filled(325, 375, 335, 285, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
 arcade.draw_lrtb_rectangle_filled(330, 370, 330, 290, arcade.color.BABY_BLUE_EYES)
 
-arcade.draw_lrtb_rectangle_filled(225, 275, 235, 185, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
-arcade.draw_lrtb_rectangle_filled(230, 270, 230, 190, arcade.color.BABY_BLUE_EYES)
+arcade.draw_lrtb_rectangle_filled(225, 275, 250, 200, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
+arcade.draw_lrtb_rectangle_filled(230, 270, 245, 205, arcade.color.BABY_BLUE_EYES)
 
-arcade.draw_lrtb_rectangle_filled(325, 375, 235, 185, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
-arcade.draw_lrtb_rectangle_filled(330, 370, 230, 190, arcade.color.BABY_BLUE_EYES)
+arcade.draw_lrtb_rectangle_filled(325, 375, 250, 200, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
+arcade.draw_lrtb_rectangle_filled(330, 370, 245, 205, arcade.color.BABY_BLUE_EYES)
+
+#creación de la puerta
+arcade.draw_lrtb_rectangle_filled(275, 325, 200, 125, arcade.color.BROWN_NOSE)
+
+#pomio de la puerta
+arcade.draw_circle_filled(320, 150, 2, arcade.color.BROWN)
 
 arcade.finish_render()
 
