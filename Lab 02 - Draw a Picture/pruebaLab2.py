4084206f11e3dd56ecabd8981febbc6f1f3af587
Chenyu Castillo <chenyu.castillo@edu.uah.es>
2022-02-17 18:03:58 +0100

    pruebaLab2

--- Lab 02 - Draw a Picture/pruebaLab2.py
+++ Lab 02 - Draw a Picture/pruebaLab2.py
@@ -6,7 +6,9 @@ arcade.set_background_color(arcade.color.BABY_BLUE)
 
 arcade.start_render()
 
+arcade.draw_circle_filled(725, 525, 50, arcade.color.ORANGE)
 
+arcade.draw_lrtb_rectangle_filled(0, 800, 100, 0)
 
 arcade.finish_render()
 
