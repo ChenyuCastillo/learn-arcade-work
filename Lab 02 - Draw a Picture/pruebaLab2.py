897222f6df59a1f6d90402d953265fc67a079718
Chenyu Castillo <chenyu.castillo@edu.uah.es>
2022-02-17 18:29:22 +0100

    pruebaLab2

--- Lab 02 - Draw a Picture/pruebaLab2.py
+++ Lab 02 - Draw a Picture/pruebaLab2.py
@@ -10,7 +10,19 @@ arcade.draw_circle_filled(725, 525, 50, arcade.color.ORANGE)
 
 arcade.draw_lrtb_rectangle_filled(0, 800, 150, 0, arcade.color.BANGLADESH_GREEN)
 
-arcade.draw_lrtb_rectangle_filled(200, 375, 350, 125, arcade.color.BLUE_GREEN)
+arcade.draw_lrtb_rectangle_filled(200, 400, 380, 125, arcade.color.BLUE_GREEN)
+
+arcade.draw_lrtb_rectangle_filled(225, 275, 335, 285, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
+arcade.draw_lrtb_rectangle_filled(230, 270, 330, 290, arcade.color.BABY_BLUE_EYES)
+
+arcade.draw_lrtb_rectangle_filled(325, 375, 335, 285, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
+arcade.draw_lrtb_rectangle_filled(330, 370, 330, 290, arcade.color.BABY_BLUE_EYES)
+
+arcade.draw_lrtb_rectangle_filled(225, 275, 235, 185, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
+arcade.draw_lrtb_rectangle_filled(230, 270, 230, 190, arcade.color.BABY_BLUE_EYES)
+
+arcade.draw_lrtb_rectangle_filled(325, 375, 235, 185, arcade.color.PURPLE_MOUNTAIN_MAJESTY)
+arcade.draw_lrtb_rectangle_filled(330, 370, 230, 190, arcade.color.BABY_BLUE_EYES)
 
 arcade.finish_render()
 
