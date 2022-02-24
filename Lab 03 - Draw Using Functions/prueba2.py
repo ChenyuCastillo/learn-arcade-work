86f63476c0a94dee3062e8f97d25728e73cce2b2
Chenyu Castillo <chenyu.castillo@edu.uah.es>
2022-02-24 18:41:46 +0100

    pruebaLab3.py

--- Lab 03 - Draw Using Functions/prueba2.py
+++ Lab 03 - Draw Using Functions/prueba2.py
@@ -4,7 +4,19 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 def base():
-    arcade.draw_lrtb_rectangle_filled(100, 700, 500, 50, arcade.color.WHITE)
+    arcade.draw_lrtb_rectangle_filled(100, 700, 400, 50, arcade.color.WHITE)
+    arcade.draw_lrtb_rectangle_filled(100, 700, 400, 100, arcade.color.GRAY)
+
+
+def disco():
+    arcade.draw_circle_filled(320, 250, 140, arcade.color.BLACK)
+    arcade.draw_circle_filled(320, 250, 100, arcade.color.WHITE)
+    arcade.draw_circle_filled(320, 250, 99, arcade.color.BLACK)
+
+
+
+
+
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
@@ -12,6 +24,7 @@ def main():
     arcade.start_render()
 
     base()
+    disco()
 
     arcade.finish_render()
     arcade.run()
