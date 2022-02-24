e72cf7c88176317e5269ee349745530a2122a806
Chenyu Castillo <chenyu.castillo@edu.uah.es>
2022-02-24 18:10:33 +0100

    pruebaLab3.py

--- Lab 03 - Draw Using Functions/prueba2.py
+++ Lab 03 - Draw Using Functions/prueba2.py
@@ -3,11 +3,15 @@ import arcade
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+def base():
+    arcade.draw_lrtb_rectangle_filled(100, 700, 500, 50, arcade.color.WHITE)
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
-    arcade.set_background_color(arcade.color.BABY_BLUE_EYES)
+    arcade.set_background_color(arcade.color.BABY_PINK)
     arcade.start_render()
+
+    base()
 
     arcade.finish_render()
     arcade.run()
